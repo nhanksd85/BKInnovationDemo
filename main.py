8bff6aa4f18c9594b15087f42ec8a46dcc654238
MSI\KAI <nhanksd85@gmail.com>
2023-10-22 10:06:34 +0700

    Add testing UI

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import time
+
 print("Hello BK Innovation Demo")
+import tkinter as tk
+from tkinter import *
+
+window = tk.Tk()
+
+window.attributes('-fullscreen', True)
+window.title("Rapido Project")
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+print("Size:", screen_width, screen_height)
+
+
+while True:
+    window.update()
+    time.sleep(0.1)
